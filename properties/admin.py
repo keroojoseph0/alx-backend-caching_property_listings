cb921b4300c65558e5df64f1605cd02634fc31a7
keroojoseph0 <keroojoseph2@.com>
2026-01-11 20:05:08 +0200

    Cache Property List View

--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Property
+from properties.models import Property
 
 # Register your models here.
+
+admin.site.register(Property)
--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Property
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
 
 # Create your views here.
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = list(Property.objects.values())
+
+    return JsonResponse(properties, safe=False)
